67d83eadeb6a1414f867706d2af4d5ebab5c5d16
Sharnell Grant <sharnellgrant@gmail.com>
2020-06-21 01:11:51 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
   
  def create
   Student.create(first_name: params[:first_name], last_name: params[:last_name])
+  redirect_to students_path
  end 
     
 end
